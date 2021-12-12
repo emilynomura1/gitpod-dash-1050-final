c09158fad93ffbc4907b0c41a4409bf35c0be786
megan-sindhi <megan_sindhi@brown.edu>
2021-12-12 23:01:49 +0000

    updating app

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
 import dash
 from dash import dcc # dash core components
-
 from dash import dash_table
-from dash import dcc # dash core components
 from dash import html
 import pandas as pd
 import numpy as np
 from dash.dependencies import Input, Output, State
+import os
+from supabase import create_client, Client
+import requests
+from bs4 import BeautifulSoup
+import datetime
+
+
+# connection to db
+url = "https://umydpehqhuatmhhyntgv.supabase.co"
+key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzOTA3NzkyNywiZXhwIjoxOTU0NjUzOTI3fQ.jwZqMtdCVOWqhc73uaxYomYvF6lbxxTLcWIQp4gSFoY"
+supabase: Client = create_client(url, key)
+
+data = supabase.table('historical_data').select('*').execute()
+historical = pd.DataFrame(data.get("data", []))
+test = pd.DataFrame()
+historical.avg_wind= pd.to_numeric(historical.avg_wind)
+historical.precipitation= historical.precipitation.astype(int)
 
 app = dash.Dash(__name__)
 
@@ -65,8 +80,101 @@ app.layout = html.Div([
             style={'fontSize':16}
         ),       
     
-    ], className="center")
+    ], className="center"),
+    html.Div(id='result'),
+    
+    html.Div([
+    dash_table.DataTable(
+    id='table',
+    columns=[{"name": str(i), "id": str(i)} for i in test.columns],
+    data=test.to_dict('records'),
+    
+    )
+    ]
+    )
+
+
 ])
 
+
+
+@app.callback(
+    [Output('result', 'children'),
+    Output(component_id='table', component_property='columns'),
+    Output(component_id='table', component_property='data')],
+    Input(component_id='submit-button', component_property='n_clicks'),
+    State(component_id='my_dropdown1', component_property='value'),
+ 
+)
+
+def pickATeam(n_clicks, input_my_dropdown1):
+    if (n_clicks == 0):
+        test = pd.DataFrame()
+        stringret = "Hello, please select a team."
+        #dispret = {'display': 'none'}
+        table_cols = [{"name": str(i), "id": str(i)} for i in test.columns]
+        table_records = test.to_dict('records')
+        return (stringret, table_cols, table_records)
+    ##reading in and formatting data
+    df = pd.read_html("http://www.nflweather.com/")[0]
+    df = df[['Away', 'Home','Time (ET)','TV', 'Forecast', "Extended Forecast",'Wind']]
+    response = requests.get("http://www.nflweather.com/")
+    soup = BeautifulSoup(response.content, "html.parser")
+    header = soup.find_all('h1')
+    week = str(header[1])
+    current_week = week.split("\n")[1].strip(" ").strip(" Forecast")
+    df['unique_event_id'] = str(datetime.date.today().year)+"-"+current_week+ "-" + df['Home'] + "-" + df['Away']
+    df[['Temp','Weather_Forecast']]=df.Forecast.str.split("f",expand=True)
+    df[['WindSpeed','toDrop']]=df.Wind.str.split("m",expand=True)
+    df['Precipitation'] = np.where(((df.Weather_Forecast.str.find("Rain") == 1) | (df.Weather_Forecast.str.find("Snow")== 1)), 1, 0)
+    df = df[['unique_event_id','Away','Home','Time (ET)','Temp','Weather_Forecast' ,'WindSpeed','Precipitation']]
+
+    if (df[(df.Home == input_my_dropdown1)|(df.Away == input_my_dropdown1)].shape[0] == 0):
+        stringret = "Your team has a bye this week."
+        #dispret = {'display': 'none'}
+        test = pd.DataFrame()
+        table_cols = [{"name": str(i), "id": str(i)} for i in test.columns]
+        table_records = test.to_dict('records')
+        return (stringret, table_cols, table_records)
+
+    finished = df[df['Time (ET)'].str.contains('Final')]
+    future = df[~(df['Time (ET)'].str.contains('Final'))]
+    ##add append code here
+    if (finished[(finished.Home == input_my_dropdown1)|(finished.Away == input_my_dropdown1)].shape[0] != 0):
+        target = finished[(finished.Away == input_my_dropdown1) | (finished.Home == input_my_dropdown1)]
+        stringret = "Your team has already played. The {} were the home team and the {} were the away team. {}".format())
+        #dispret = {'display': 'none'}
+        test = pd.DataFrame()
+        table_cols = [{"name": str(i), "id": str(i)} for i in test.columns]
+        table_records = test.to_dict('records')
+        return (stringret, table_cols, table_records)
+    
+    target = future[(future.Away == input_my_dropdown1) | (future.Home == input_my_dropdown1)]
+    team = historical[(historical.home == input_my_dropdown1)|(historical.away == input_my_dropdown1)]
+    if (target.Temp.values[0] == "DOME"):
+        comparable = team[team.stadium=='dome']
+        x = round(comparable[comparable.winner == input_my_dropdown1].shape[0]/comparable.shape[0],2)
+        percentage = "{:.0%}".format(x)
+        stringret = "Your team is playing in a dome. In the past they have won {} percent of games they have played in a dome".format(percentage)
+        #dispret = {'display': 'block'}
+        table_cols = [{"name": str(i), "id": str(i)} for i in comparable.columns]
+        table_records = comparable.to_dict('records')
+        
+    else:
+        target.Temp = target.Temp.astype(float)
+        target.WindSpeed = target.WindSpeed.astype(float)
+        uptemp = (target.Temp.values[0]+5)
+        lowtemp = (target.Temp.values[0]-5)
+        upwind = (target.WindSpeed.values[0]+5)
+        lowwind = (target.WindSpeed.values[0]-5)
+        comparable = team[(team.avg_temp > lowtemp)&(team.avg_temp < uptemp)&(team.precipitation==target.Precipitation.values[0])&(team.avg_wind < upwind )&(team.avg_wind > lowwind )]
+        x = round(comparable[comparable.winner == input_my_dropdown1].shape[0]/comparable.shape[0],2)
+        percentage = "{:.0%}".format(x)
+    
+        stringret = "Your team has won {} percent of games in comparable weather conditions.".format(percentage)
+        #dispret = {'display': 'block'}
+        table_cols = [{"name": str(i), "id": str(i)} for i in comparable.columns]
+        table_records = comparable.to_dict('records')
+    return (stringret, table_cols, table_records)
 
 app.run_server(debug=True, host="0.0.0.0")
