d90814330e0febfda840194032253639e814ab81
Jason Hooker <53316888+jchooker18@users.noreply.github.com>
2021-12-13 16:34:18 -0500

    Added db interactions to app

--- app.py
+++ app.py
@@ -17,11 +17,67 @@ url = "https://umydpehqhuatmhhyntgv.supabase.co"
 key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzOTA3NzkyNywiZXhwIjoxOTU0NjUzOTI3fQ.jwZqMtdCVOWqhc73uaxYomYvF6lbxxTLcWIQp4gSFoY"
 supabase: Client = create_client(url, key)
 
+# get historical data
 data = supabase.table('historical_data').select('*').execute()
 historical = pd.DataFrame(data.get("data", []))
 test = pd.DataFrame()
 historical.avg_wind= pd.to_numeric(historical.avg_wind)
 historical.precipitation= historical.precipitation.astype(int)
+
+#reading in website data
+df = pd.read_html("http://www.nflweather.com/")[0]
+
+# fill na's and add a timestamp 
+df = df.fillna(0)
+df['date_pulled'] = str(datetime.datetime.now())
+
+# convert to dict, nsert to raw_data db
+dfd = df.to_dict('records')
+supabase.table('raw_data').insert(dfd).execute()
+assert data.get("status_code") in (200, 201)
+
+# clean data
+df = df[['Away', 'Home','Time (ET)','TV', 'Forecast', "Extended Forecast",'Wind']]
+response = requests.get("http://www.nflweather.com/")
+soup = BeautifulSoup(response.content, "html.parser")
+header = soup.find_all('h1')
+week = str(header[1])
+current_week = week.split("\n")[1].strip(" ").strip(" Forecast")
+df['unique_event_id'] = str(datetime.date.today().year)+"-"+current_week+ "-" + df['Home'] + "-" + df['Away']
+df[['Temp','Weather_Forecast']]=df.Forecast.str.split("f",expand=True)
+df[['WindSpeed','toDrop']]=df.Wind.str.split("m",expand=True)
+df['Precipitation'] = np.where(((df.Weather_Forecast.str.find("Rain") == 1) | (df.Weather_Forecast.str.find("Snow")== 1)), 1, 0)
+df = df[['unique_event_id','Away','Home','Time (ET)','Temp','Weather_Forecast' ,'WindSpeed','Precipitation']]
+
+finished = df[df['Time (ET)'].str.contains('Final')]
+future = df[~(df['Time (ET)'].str.contains('Final'))]
+
+# get the processed data, load into df
+data = supabase.table('processed_data').select('*').execute()
+processed = pd.DataFrame(data.get("data", []))
+
+rows_to_add = []
+
+# iterate through rows
+for index, row in finished.iterrows():
+    
+    # check if processed is empty
+    if len(processed) == 0:
+        rowd = row.to_dict()
+        rows_to_add.append(rowd)
+    
+    # if unique event id already in processed, continue
+    elif row['unique_event_id'] in processed['unique_event_id'].unique():
+        continue
+    
+    # else, add the row to list of rows to append
+    else:
+        rowd = row.to_dict()
+        rows_to_add.append(rowd)
+        
+# insert to db
+supabase.table('processed_data').insert(rows_to_add).execute()
+assert data.get("status_code") in (200, 201)
 
 app = dash.Dash(__name__)
 
@@ -51,6 +107,7 @@ app.layout = html.Div([
     {"label": "Detroit Lions", "value": "Lions"},
     {"label": "Green Bay Packers", "value": "Packers"},
     {"label": "Houston Texans", "value": "Texans"},
+    {"label": "Indianapolis Colts", "value": "Colts"},
     {"label": "Jacksonville Jaguars", "value": "Jaguars"},
     {"label": "Kansas City Chiefs", "value": "Chiefs"},
     {"label": "Las Vegas Raiders", "value": "Raiders"},
@@ -66,14 +123,14 @@ app.layout = html.Div([
     {"label": "Pittsburgh Steelers", "value": "Steelers"},
     {"label": "San Francisco 49ers", "value": "49ers"},
     {"label": "Seattle Seahawks", "value": "Seahawks"},
-    {"label": "Tampa Bay Buccanears", "value": "Buccanears"},
+    {"label": "Tampa Bay Buccaneers", "value": "Buccaneers"},
     {"label": "Tennessee Titans", "value": "Titans"},
     {"label": "Washington Football Team", "value": "Washington"},
             ],
             value='Select a team'
         ),
         html.Label(id='my_label1'),
- html.Button(
+    html.Button(
             id='submit-button',
             n_clicks=0,
             children='Submit',
@@ -84,16 +141,12 @@ app.layout = html.Div([
     html.Div(id='result'),
     
     html.Div([
-    dash_table.DataTable(
-    id='table',
-    columns=[{"name": str(i), "id": str(i)} for i in test.columns],
-    data=test.to_dict('records'),
-    
-    )
-    ]
-    )
-
-
+        dash_table.DataTable(
+            id='table',
+            columns=[{"name": str(i), "id": str(i)} for i in test.columns],
+            data=test.to_dict('records')
+        )
+    ])
 ])
 
 
@@ -115,20 +168,7 @@ def pickATeam(n_clicks, input_my_dropdown1):
         table_cols = [{"name": str(i), "id": str(i)} for i in test.columns]
         table_records = test.to_dict('records')
         return (stringret, table_cols, table_records)
-    ##reading in and formatting data
-    df = pd.read_html("http://www.nflweather.com/")[0]
-    df = df[['Away', 'Home','Time (ET)','TV', 'Forecast', "Extended Forecast",'Wind']]
-    response = requests.get("http://www.nflweather.com/")
-    soup = BeautifulSoup(response.content, "html.parser")
-    header = soup.find_all('h1')
-    week = str(header[1])
-    current_week = week.split("\n")[1].strip(" ").strip(" Forecast")
-    df['unique_event_id'] = str(datetime.date.today().year)+"-"+current_week+ "-" + df['Home'] + "-" + df['Away']
-    df[['Temp','Weather_Forecast']]=df.Forecast.str.split("f",expand=True)
-    df[['WindSpeed','toDrop']]=df.Wind.str.split("m",expand=True)
-    df['Precipitation'] = np.where(((df.Weather_Forecast.str.find("Rain") == 1) | (df.Weather_Forecast.str.find("Snow")== 1)), 1, 0)
-    df = df[['unique_event_id','Away','Home','Time (ET)','Temp','Weather_Forecast' ,'WindSpeed','Precipitation']]
-
+    
     if (df[(df.Home == input_my_dropdown1)|(df.Away == input_my_dropdown1)].shape[0] == 0):
         stringret = "Your team has a bye this week."
         #dispret = {'display': 'none'}
@@ -137,12 +177,12 @@ def pickATeam(n_clicks, input_my_dropdown1):
         table_records = test.to_dict('records')
         return (stringret, table_cols, table_records)
 
-    finished = df[df['Time (ET)'].str.contains('Final')]
-    future = df[~(df['Time (ET)'].str.contains('Final'))]
-    ##add append code here
     if (finished[(finished.Home == input_my_dropdown1)|(finished.Away == input_my_dropdown1)].shape[0] != 0):
         target = finished[(finished.Away == input_my_dropdown1) | (finished.Home == input_my_dropdown1)]
-        stringret = "Your team has already played. The {} were the home team and the {} were the away team.".format(str(target['Home']).split()[1], str(target['Away']).split()[1])
+        home = target['Home'].iat[0]
+        away = target['Away'].iat[0]
+        score = target['Time (ET)'].iat[0]
+        stringret = "Your team has already played. The {} were the away team and the {} were the home team. {}".format(away, home, score)
         #dispret = {'display': 'none'}
         test = pd.DataFrame()
         table_cols = [{"name": str(i), "id": str(i)} for i in test.columns]
